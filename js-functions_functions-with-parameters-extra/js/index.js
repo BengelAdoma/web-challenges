908cb434943acbe9a064542780ecaad3e2a1f5cd
Bengel Adoma <bengelansah@gmail.com>
2024-12-17 16:31:18 +0100

    add all

--- js-functions_functions-with-parameters-extra/js/index.js
+++ js-functions_functions-with-parameters-extra/js/index.js
@@ -6,7 +6,12 @@ console.clear();
    The formula is: square = number * number
 */
 
-function printSquare(number) {}
+function printSquare(number) {
+  console.log(number * number); 
+   
+  
+}
+
 
 printSquare(3);
 printSquare(5);
@@ -18,9 +23,13 @@ printSquare(5);
 */
 
 // Uncomment the following function calls and implement the function `printCircumference`.
-
-// printCircumference(4);
-// printCircumference(6);
+function printCircumference(number) {
+   console.log(number * 3.142 * 2); 
+    
+   
+ }
+printCircumference(4);
+ printCircumference(6);
 
 /*
 3: Write a function that accepts the width and length of a rectangle
@@ -31,6 +40,10 @@ printSquare(5);
 */
 
 // Uncomment the following function calls and implement the function `printRectangleArea`.
-
-// printRectangleArea(5, 7);
-// printRectangleArea(3, 4);
+function printRectangleArea(num1, num2) {
+   console.log("The area of the rectangle is" , num1 * num2); 
+    
+   
+ }
+ printRectangleArea(5, 7);
+printRectangleArea(3, 4);
