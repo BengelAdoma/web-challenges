477ea0572d013f701af301644783071bdd210113
Bengel Adoma <bengelansah@gmail.com>
2025-01-15 15:23:57 +0100

    done

--- react-challenges/react-nesting_fragments/src/App.jsx
+++ react-challenges/react-nesting_fragments/src/App.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./styles.css";
 import Box from "./components/Box";
 
 export default function App() {
   return (
     <main className="flex-container">
-      <Box color="#007bff" />
-      <Box color="#fc3" />
-      <Box color="#ff3333" />
+      <Boxes/>
+      <Boxes/>
+      <Boxes/>
     </main>
   );
 }
+ function Boxes(){
+  return <Fragment>
+    <Box color="#007bff" />
+      <Box color="#fc3" />
+      <Box color="#ff3333" />
+  </Fragment>
+ }
